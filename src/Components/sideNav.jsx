a037d8dcc312c5e500a7d15b0a633d39cb684016
The-Byte-Bandit <sundaykevinneza@gmail.com>
2024-12-12 23:38:15 +0100

    settings almost done

--- src/Components/sideNav.jsx
+++ src/Components/sideNav.jsx
@@ -1,5 +1,5 @@
-import { arrowRight, sideNavLinks, Swiphr, tip, userProfile, verified } from '../constants/constant';
-import styles from '../style';
+import { arrowRight, sideNavLinks, Swiphr, tip, userProfile, verified, userset, settings_wallet, notification_seticon, privAndsec, arro, arrowDown  } from '../constants/constant';
+import styles, { typography } from '../style.js';
 import { useSelector, useDispatch } from 'react-redux';
 import { activate } from '../features/sideNav';
 import { Link, useLocation } from 'react-router-dom';
@@ -12,6 +12,8 @@ function SideNav() {
 
   // State for toggling the mini navigation
   const [miniNavVisible, setMiniNavVisible] = useState(false);
+  const [settingsDropdownVisible, setSettingsDropdownVisible] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     sideNavLinks.forEach((link) => {
@@ -19,7 +21,40 @@ function SideNav() {
         dispatch(activate(link.id));
       }
     });
+    setSettingsDropdownVisible(location.pathname.includes('/settings'));
   }, [location, dispatch]); // Re-run when location changes
+
+  useEffect(() => {
+    // Disable scrolling when the side nav is visible
+    if (miniNavVisible) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = ''; // Restore default scrolling
+    }
+
+    // Cleanup on unmount
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [miniNavVisible]);
+
+  useEffect(() => {
+    if (location.pathname.includes('account-settings')) {
+      setActiveIndex(1);
+    } else if (location.pathname.includes('payment-settings')) {
+      setActiveIndex(2);
+    } else if (location.pathname.includes('notification-settings')) {
+      setActiveIndex(3);
+    } else if (location.pathname.includes('privacy-settings')) {
+      setActiveIndex(4);
+    }
+  }, [location]);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  }
+
+
 
   return (
     <div id='sidenav'>
@@ -44,7 +79,7 @@ function SideNav() {
           </div>
 
           {/* Links Section */}
-          <div className={`${miniNavVisible ? 'flex ' : 'hidden'} flex-col gap-2 px-4`}>
+          {/* <div className={`${miniNavVisible ? 'flex ' : 'hidden'} flex-col gap-2 px-4`}>
             <ul className='flex flex-col gap-3'>
               {sideNavLinks.map((link) => (
                 <li
@@ -63,6 +98,146 @@ function SideNav() {
                     )}
                     <span className='label-small'>{link.title}</span>
                   </Link>
+                </li>
+              ))}
+            </ul>
+          </div> */}
+
+                 {/* Links Section */}
+          <div className={`${miniNavVisible ? 'flex ' : 'hidden'} flex-col gap-2 px-4`}>
+            <ul className="flex flex-col gap-3">
+              {sideNavLinks.map((link) => (
+                <li
+                  key={link.id}
+                  onClick={() => {
+                    dispatch(activate(link.id));
+                    if (link.url !== 'settings') setMiniNavVisible(false);
+                  }}
+                  className={`flex flex-col gap-4 cursor-pointer hover:bg-weak-100 p-2 rounded-md ${
+                    location.pathname.includes(link.url) ? 'bg-weak-100' : ''
+                  }`}
+                >
+                  {link.url === 'settings' ? (
+                    <>
+                      <div
+                         onClick={() => {
+                          setSettingsDropdownVisible(!settingsDropdownVisible);
+                          }}
+                        className="flex gap-3 w-full items-center"
+                      >
+                        <img
+                          src={itemState[link.id] ? link.activeIcon : link.icon}
+                          alt={link.title}
+                        />
+                        <span className="label-small">{link.title}</span>
+                        <span className="flex items-center h-full ml-auto">
+                          <div><img src={arrowDown} alt='Down arrow'/></div>
+                        </span>
+                      </div>
+                      {settingsDropdownVisible && (
+  <div
+    className={`flex flex-col font-semibold text-text-soft-400 settings-dropdown`}
+  >
+    <Link to="/home/settings/account-settings">
+      <div
+        className={`${
+          activeIndex === 1 ? 'active' : ''
+        } pl-[8px] py-[8px] flex justify-between hover:bg-weak-100 rounded-md`}
+        onClick={() => {
+          handleClick(1);
+          setMiniNavVisible(!miniNavVisible);
+        }}
+      >
+        <div className="flex items-center w-full">
+          <img
+            className="cgrey pr-2"
+            src={userset}
+            alt="Account Settings"
+          />
+          <h5 className={`${typography.labelSmall}`}>Account Settings</h5>
+        </div>
+        <img className="arr" src={arro} alt="arrow" />
+      </div>
+    </Link>
+
+    <Link to="/home/settings/payment-settings">
+      <div
+        className={`${
+          activeIndex === 2 ? 'active' : ''
+        } pl-[8px] py-[8px] flex justify-between hover:bg-weak-100 rounded-md`}
+        onClick={() => {
+          handleClick(2);
+          setMiniNavVisible(!miniNavVisible);
+        }}
+      >
+        <div className="flex items-center w-full">
+          <img
+            className="cgrey pr-2"
+            src={settings_wallet}
+            alt="Payment Settings"
+          />
+          <h5 className={`${typography.labelSmall}`}>Payment Settings</h5>
+        </div>
+        <img className="arr" src={arro} alt="arrow" />
+      </div>
+    </Link>
+
+    <Link to="/home/settings/notification-settings">
+      <div
+        className={`${
+          activeIndex === 3 ? 'active' : ''
+        } pl-[8px] py-[8px] flex justify-between hover:bg-weak-100 rounded-md`}
+        onClick={() => {
+          handleClick(3);
+          setMiniNavVisible(!miniNavVisible);
+        }}
+      >
+        <div className="flex items-center w-full">
+          <img
+            className="cgrey pr-2"
+            src={notification_seticon}
+            alt="Notification Settings"
+          />
+          <h5 className={`${typography.labelSmall}`}>Notification Settings</h5>
+        </div>
+        <img className="arr" src={arro} alt="arrow" />
+      </div>
+    </Link>
+
+    <Link to="/home/settings/privacy-settings">
+      <div
+        className={`${
+          activeIndex === 4 ? 'active' : ''
+        } pl-[8px] py-[8px] flex justify-between hover:bg-weak-100 rounded-md`}
+        onClick={() => {
+          handleClick(4);
+          setMiniNavVisible(!miniNavVisible);
+        }}
+      >
+        <div className="flex items-center w-full">
+          <img
+            className="cgrey pr-2"
+            src={privAndsec}
+            alt="Privacy & Security"
+          />
+          <h5 className={`${typography.labelSmall}`}>Privacy & Security</h5>
+        </div>
+        <img className="arr" src={arro} alt="arrow" />
+      </div>
+    </Link>
+  </div>
+)}
+
+                    </>
+                  ) : (
+                    <Link to={`/home/${link.url}`} className="flex gap-3 items-center">
+                      <img
+                        src={itemState[link.id] ? link.activeIcon : link.icon}
+                        alt={link.title}
+                      />
+                      <span className="label-small">{link.title}</span>
+                    </Link>
+                  )}
                 </li>
               ))}
             </ul>
@@ -85,7 +260,7 @@ function SideNav() {
         </div>
       </div>
 
-      <div className={`${miniNavVisible ? 'flex' : 'hidden'} w-screen h-screen bg-[#00000081] ease-linear absolute left-0 z-10 transition-opacity duration-300 top-0`}></div>
+      <div className={`${miniNavVisible ? 'flex' : 'hidden'} w-screen min-h-screen  bg-[#00000081] ease-linear absolute left-0 z-10 transition-opacity duration-300 top-0`}></div>
       
       {/* {miniNavVisible && (
           <div
